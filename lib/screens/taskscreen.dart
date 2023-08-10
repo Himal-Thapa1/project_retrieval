7856f9f2287b81069235a8a8be928bbdf21774f4
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-08-10 10:38:40 +0545

    added icon and text todo to the screen

--- lib/screens/taskscreen.dart
+++ lib/screens/taskscreen.dart
@@ -5,6 +5,38 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50, left: 30, right: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30,
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'ToDo',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
