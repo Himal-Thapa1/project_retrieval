e495dadb9699c1def79bb7b0dd2bb5b33511433d
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-08-10 17:12:35 +0545

    os reinstalled

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:project_retrieval/widgets/task_tile.dart';
+import '../models/task.dart';
 
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     super.key,
   });
+
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+
+  List<Task> task = [
+    Task(name: "do TASK 1"),
+    Task(name: "Do task 2"),
+    Task(name: "Do task 3"),
+  ];
 
   @override
   Widget build(BuildContext context) {
