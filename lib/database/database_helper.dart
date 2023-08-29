32e2233b7629add5372ce7b041755bf2e05c9eac
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-08-29 11:55:01 +0545

    everything works now

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'tasks.db');
+
+    return await openDatabase(path, version: 1, onCreate: _createDatabase);
+  }
+
+  Future<void> _createDatabase(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE tasks(id INTEGER PRIMARY KEY, name TEXT, isDone INTEGER)',
+    );
+  }
+}
--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,18 +1,57 @@
+
 import 'dart:collection';
 
-import 'package:project_retrieval/models/task.dart';
 import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> _tasks = [
-    Task(name: "Add your todo list here."),
-    Task(name: "Long press the task to delete the task."),
-  ];
+  late Database _database;
+  List<Task> _tasks = [];
 
   TaskData() {
-    _loadTasks();
+    _initDatabase();
   }
+
+  Future<void> _initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+  final path = join(databasesPath, 'tasks.db');
+  _database = await openDatabase(path, version: 1, onCreate: _createDatabase);
+
+  final hasTasks = await _hasTasks();
+
+  if (!hasTasks) {
+    final initialTasks = [
+      Task(name: "Add your todo list here."),
+      Task(name: "Long press the task to delete the task."),
+    ];
+
+    for (final task in initialTasks) {
+      await _database.insert(
+        'tasks',
+        task.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.ignore,
+      );
+    }
+  }
+
+  _loadTasks();
+  }
+
+  Future<bool> _hasTasks() async {
+  final count = Sqflite.firstIntValue(
+    await _database.rawQuery('SELECT COUNT(*) FROM tasks'),
+  );
+
+  return count! > 0;
+}
+
+  Future<void> _createDatabase(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE tasks(id INTEGER PRIMARY KEY, name TEXT, isDone INTEGER)',
+    );
+  }
+
   UnmodifiableListView<Task> get tasks {
     return UnmodifiableListView(_tasks);
   }
@@ -21,52 +60,68 @@ class TaskData extends ChangeNotifier {
     return _tasks.length;
   }
 
-  void addTask(String newTaskTitle) {
+  Future<void> addTask(String newTaskTitle) async {
     final task = Task(name: newTaskTitle);
-    _tasks.add(task);
-    _saveTasks();
-    notifyListeners();
+    await _database.insert(
+      'tasks',
+      task.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    _loadTasks();
   }
 
-  void updateTask(Task task) {
+  Future<void> updateTask(Task task) async {
     task.toggleDone();
-    _saveTasks();
-    notifyListeners();
+    await _database.update(
+      'tasks',
+      task.toMap(),
+      where: 'id = ?',
+      whereArgs: [task.name.hashCode],
+    );
+    _loadTasks();
   }
 
-  void deleteTask(Task task) {
-    _tasks.remove(task);
-    _saveTasks();
-    notifyListeners();
-  }
-
-  // void updateTasksFromStrings(List<String> taskInfoList) {
-  //   _tasks = taskInfoList.map((taskInfo) {
-  //     final taskInfoParts = taskInfo.split(',');
-  //     return Task(
-  //       name: taskInfoParts[0],
-  //       isDone: taskInfoParts[1] == 'true', // Convert the string to a boolean
-  //     );
-  //   }).toList();
-  //   notifyListeners();
-  // }
-
-  Future<void> _saveTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    for (final task in _tasks) {
-      await prefs.setBool(task.name, task.isDone);
-    }
+  Future<void> deleteTask(Task task) async {
+    await _database.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [task.name.hashCode],
+    );
+    _loadTasks();
   }
 
   Future<void> _loadTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    final taskNames = prefs.getKeys().toList();
-    _tasks = taskNames.map((taskName) {
-      return Task(
-        name: taskName,
-        isDone: prefs.getBool(taskName) ?? false,
-      );
+    final List<Map<String, dynamic>> rows =
+        await _database.query('tasks', orderBy: 'id');
+    _tasks = rows.map((row) {
+      return Task.fromMap(row);
     }).toList();
     notifyListeners();
+  }
+}
+
+class Task {
+  final String name;
+  bool isDone;
+
+  Task({required this.name, this.isDone = false});
+
+  void toggleDone() {
+    isDone = !isDone;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': name.hashCode,
+      'name': name,
+      'isDone': isDone ? 1 : 0,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      name: map['name'],
+      isDone: map['isDone'] == 1,
+    );
   }
 }
